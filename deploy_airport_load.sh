6ddf27dbc0bbc925bcf8048f6e5df14e6e3e0f9e
Vasily Baydin <v.baydin@s7.ru>
2021-01-25 18:22:35 +0300

    update deployments

--- deploy_airport_load.sh
+++ deploy_airport_load.sh
@@ -2,4 +2,4 @@
 # read -s -p "Enter Google Client SECRET: " secret
 # docker-compose build
 . google_env
-GOOGLE_CLIENT_ID=${id} GOOGLE_CLIENT_SECRET=${secret} docker-compose up 
+GOOGLE_CLIENT_ID=${id} GOOGLE_CLIENT_SECRET=${secret} docker-compose -f airport-load.docker-compose.yaml up 
---
+++ deploy_portal.sh
@@ -0,0 +1,5 @@
+# read -s -p "Enter Google Client ID: " id
+# read -s -p "Enter Google Client SECRET: " secret
+# docker-compose build
+. google_env
+GOOGLE_CLIENT_ID=${id} GOOGLE_CLIENT_SECRET=${secret} docker-compose -f portal.docker-compose.yaml up 
